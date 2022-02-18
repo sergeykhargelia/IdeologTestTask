e7a057eb663519522808ace09ec1e3cceef0710b
sergeykhargelia <sergey.khargelia@gmail.com>
2022-02-18 04:18:05 +0300

    added comments

--- src/main/kotlin/LogStatistics.kt
+++ src/main/kotlin/LogStatistics.kt
@@ -7,6 +7,25 @@ import java.io.PrintWriter
 const val threadNamePosition = 5
 const val mostFrequentCount = 10
 
+/**
+ * This function takes the log as Sequence<String>
+ * and returns a list of pairs, representing most frequent threads.
+ * Each pair consists of the thread name and the number of lines in the log
+ * with this thread
+ */
+
+fun getMostFrequentThreads(data: Sequence<String>): List<Pair<String, Int>> {
+    val logsByThread = data.groupBy {
+        it.split(Regex("\\s+")).getOrElse(threadNamePosition) {
+            throw IllegalArgumentException("Wrong structure of log file")
+        }
+    }
+
+    return logsByThread.toList().map { (thread, logs) ->
+        Pair (thread, logs.size)
+    }.sortedByDescending { (_, count) -> count }.take(mostFrequentCount)
+}
+
 fun main(args: Array<String>) {
     try {
         if (args.isEmpty()) {
@@ -18,15 +37,7 @@ fun main(args: Array<String>) {
         }
 
         File(inputFileName).useLines { data ->
-            val logsByThread = data.groupBy {
-                it.split(Regex("\\s+")).getOrElse(threadNamePosition) {
-                    throw IllegalArgumentException("Wrong structure of log file")
-                }
-            }
-
-            val mostFrequentThreads = logsByThread.toList().map { (thread, logs) ->
-                Pair (thread, logs.size)
-            }.sortedByDescending { (_, count) -> count }.take(mostFrequentCount)
+            val mostFrequentThreads = getMostFrequentThreads(data)
 
             if (args.size >= 2) {
                 val outputFileName = args[1]
